a73e9aad1f0b27ddf44ff5eae1256503b67e5911
alexmansour-root <75757227+alexmansour-root@users.noreply.github.com>
2021-03-15 13:37:04 -0700

    Add files via upload

---
+++ Treasure Island.py
@@ -0,0 +1,43 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice = input("""You're at a cross road. Where do you want to go? Type "left" or "right"\n""")
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+if choice == "left":
+  next_choice = input(""" You've come to a lake. There is an island in the middle. Type "wait" to wait for a boat. Type "swim" to swim the lake.\n""")
+  if next_choice == "swim":
+    print("Sorry, you were eaten by a shark. Game over!")
+  elif next_choice == "wait":
+    last_choice = input("You arrive to the island unharmed. You find three doors, one red, one yellow, and one blue. Which do you choose?\n")
+    if last_choice == "blue":
+      print("Sorry, the blue Goblins found you. Game Over!")
+    elif last_choice == "red":
+      print("Sorry, you got burned by fire. Game Over!")
+    elif last_choice == "yellow":
+      print("You found the treasure. You Win!")
+
+else:
+  print("Sorry, you fell into a hole. Game Over!")
+
